f7f0786163ffac979e33d56fa8ce18f78d35a706
Imad Ahmad <imadahmad97@yahoo.ca>
2024-11-25 00:28:54 -0800

    finished level 2 integration tests

--- app/process_response/test_process_response.py
+++ app/process_response/test_process_response.py
@@ -17,7 +17,7 @@ class TestProcessAPICall(unittest.TestCase):
 
     @freeze_time("2024-01-01 10:00:00", tz_offset=0)
     @patch("requests.get")  # Mock requests.get
-    def test_mocked_api_response(self, mock_get):
+    def test_no_adjustments(self, mock_get):
         # Create a mock response object
         mock_response = MagicMock()
         mock_response.status_code = 200
@@ -75,11 +75,11 @@ class TestProcessAPICall(unittest.TestCase):
         mock_response.status_code = 200
         mock_response.json.return_value = {
             "results": {
-                "sunrise": "11:30:00 PM",
+                "sunrise": "12:30:00 AM",
                 "sunset": "7:00:00 PM",
                 "solar_noon": "10:00:00 PM",
                 "day_length": "10:46:18",
-                "civil_twilight_begin": "12:30:00 AM",
+                "civil_twilight_begin": "11:30:00 PM",
                 "civil_twilight_end": "8:00:00 PM",
                 "nautical_twilight_begin": "5:55:00 PM",
                 "nautical_twilight_end": "2:58:54 AM",
@@ -96,14 +96,115 @@ class TestProcessAPICall(unittest.TestCase):
         process_api = ProcessAPICall()
         # Call your function that processes the API response
         response = process_api.process_api_call(mock_response)
-
         expected_response = SunTimes(
-            sunrise=datetime(2024, 1, 1, 5, 0, 0, tzinfo=timezone.utc),
-            sunset=datetime(2024, 1, 1, 19, 0, 0, tzinfo=timezone.utc),
-            morning_twilight=datetime(2024, 1, 1, 4, 30, 0, tzinfo=timezone.utc),
-            night_twilight=datetime(2024, 1, 1, 20, 0, 0, tzinfo=timezone.utc),
-            midday_period_begins=datetime(2024, 1, 1, 5, 30, 0, tzinfo=timezone.utc),
-            midday_period_ends=datetime(2024, 1, 1, 18, 0, 0, tzinfo=timezone.utc),
+            sunrise=datetime(2024, 1, 2, 0, 30, 0, tzinfo=timezone.utc),
+            sunset=datetime(2024, 1, 2, 19, 0, 0, tzinfo=timezone.utc),
+            morning_twilight=datetime(2024, 1, 1, 23, 30, 0, tzinfo=timezone.utc),
+            night_twilight=datetime(2024, 1, 2, 20, 00, 0, tzinfo=timezone.utc),
+            midday_period_begins=datetime(2024, 1, 2, 1, 30, 0, tzinfo=timezone.utc),
+            midday_period_ends=datetime(2024, 1, 2, 18, 0, 0, tzinfo=timezone.utc),
+            user_time=datetime(2024, 1, 1, 10, 0, 0, tzinfo=timezone.utc),
+        )
+        # Assertions
+        self.assertEqual(response.sunrise, expected_response.sunrise)
+        self.assertEqual(response.sunset, expected_response.sunset)
+        self.assertEqual(response.morning_twilight, expected_response.morning_twilight)
+        self.assertEqual(response.night_twilight, expected_response.night_twilight)
+        self.assertEqual(
+            response.midday_period_begins, expected_response.midday_period_begins
+        )
+        self.assertEqual(
+            response.midday_period_ends, expected_response.midday_period_ends
+        )
+        self.assertEqual(response.user_time, expected_response.user_time)
+
+    @freeze_time("2024-01-01 10:00:00", tz_offset=0)
+    @patch("requests.get")  # Mock requests.get
+    def test_sunset_before_sunrise_adjustment(self, mock_get):
+        # Create a mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "results": {
+                "sunrise": "10:30:00 PM",
+                "sunset": "6:30:00 AM",
+                "solar_noon": "10:00:00 PM",
+                "day_length": "10:46:18",
+                "civil_twilight_begin": "10:00:00 PM",
+                "civil_twilight_end": "7:30:00 AM",
+                "nautical_twilight_begin": "5:55:00 PM",
+                "nautical_twilight_end": "2:58:54 AM",
+                "astronomical_twilight_begin": "6:24:17 PM",
+                "astronomical_twilight_end": "2:29:37 AM",
+            },
+            "status": "OK",
+            "tzid": "UTC",
+        }
+
+        # Assign the mock response to requests.get
+        mock_get.return_value = mock_response
+
+        process_api = ProcessAPICall()
+        # Call your function that processes the API response
+        response = process_api.process_api_call(mock_response)
+        expected_response = SunTimes(
+            sunrise=datetime(2024, 1, 1, 22, 30, 0, tzinfo=timezone.utc),
+            sunset=datetime(2024, 1, 2, 6, 30, 0, tzinfo=timezone.utc),
+            morning_twilight=datetime(2024, 1, 1, 22, 0, 0, tzinfo=timezone.utc),
+            night_twilight=datetime(2024, 1, 2, 7, 30, 0, tzinfo=timezone.utc),
+            midday_period_begins=datetime(2024, 1, 1, 23, 0, 0, tzinfo=timezone.utc),
+            midday_period_ends=datetime(2024, 1, 2, 5, 30, 0, tzinfo=timezone.utc),
+            user_time=datetime(2024, 1, 1, 10, 0, 0, tzinfo=timezone.utc),
+        )
+        # Assertions
+        self.assertEqual(response.sunrise, expected_response.sunrise)
+        self.assertEqual(response.sunset, expected_response.sunset)
+        self.assertEqual(response.morning_twilight, expected_response.morning_twilight)
+        self.assertEqual(response.night_twilight, expected_response.night_twilight)
+        self.assertEqual(
+            response.midday_period_begins, expected_response.midday_period_begins
+        )
+        self.assertEqual(
+            response.midday_period_ends, expected_response.midday_period_ends
+        )
+        self.assertEqual(response.user_time, expected_response.user_time)
+
+    @freeze_time("2024-01-01 10:00:00", tz_offset=0)
+    @patch("requests.get")  # Mock requests.get
+    def test_night_twilight_before_sunset_adjustment(self, mock_get):
+        # Create a mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "results": {
+                "sunrise": "4:30:00 PM",
+                "sunset": "11:30:00 PM",
+                "solar_noon": "10:00:00 PM",
+                "day_length": "10:46:18",
+                "civil_twilight_begin": "3:30:00 PM",
+                "civil_twilight_end": "12:30:00 AM",
+                "nautical_twilight_begin": "5:55:00 PM",
+                "nautical_twilight_end": "2:58:54 AM",
+                "astronomical_twilight_begin": "6:24:17 PM",
+                "astronomical_twilight_end": "2:29:37 AM",
+            },
+            "status": "OK",
+            "tzid": "UTC",
+        }
+
+        # Assign the mock response to requests.get
+        mock_get.return_value = mock_response
+
+        process_api = ProcessAPICall()
+        # Call your function that processes the API response
+        response = process_api.process_api_call(mock_response)
+        expected_response = SunTimes(
+            sunrise=datetime(2024, 1, 1, 16, 30, 0, tzinfo=timezone.utc),
+            sunset=datetime(2024, 1, 1, 23, 30, 0, tzinfo=timezone.utc),
+            morning_twilight=datetime(2024, 1, 1, 15, 30, 0, tzinfo=timezone.utc),
+            night_twilight=datetime(2024, 1, 2, 0, 30, 0, tzinfo=timezone.utc),
+            midday_period_begins=datetime(2024, 1, 1, 17, 30, 0, tzinfo=timezone.utc),
+            midday_period_ends=datetime(2024, 1, 1, 22, 30, 0, tzinfo=timezone.utc),
             user_time=datetime(2024, 1, 1, 10, 0, 0, tzinfo=timezone.utc),
         )
         # Assertions
